9df79e05a41aa2d98e933561a98f5cea5b90c90d
MartinDetloff <122770564+MartinDetloff@users.noreply.github.com>
2025-05-14 21:25:15 -0600

    got the model working on training

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
+from keras import Sequential
+from keras.src.layers import Flatten, Dense, GlobalAveragePooling2D
 from tensorflow import keras
 import tensorflow as tf
 from tensorflow.keras.applications import ResNet50
 import numpy as np
-# tf.enable_eager_execution()
-
+import kagglehub
 
 
 # Method to print out the class labels
@@ -14,8 +15,6 @@ def print_class_labels(dataset, isTest):
     else:
         print("Train Data Class Labels")
 
-
-
     for i, class_label in enumerate(dataset.class_names):
         print("This is the class label:", i , " ",  class_label)
 
@@ -23,50 +22,60 @@ def print_class_labels(dataset, isTest):
 
 
 if __name__ == '__main__':
+    print("Starting Main... ")
 
-    data_test_dir = "C:/Users/Martin/Documents/GitHub/birdClassifier/data/archive/Test"
-    data_train_dir = "C:/Users/Martin/Documents/GitHub/birdClassifier/data/archive/Train"
+    print("TensorFlow version:", tf.__version__)
+    print("GPUs available:", tf.config.list_physical_devices('GPU'))
+
+    # Use kaggle's API to download the dataset from the website
+    path = kagglehub.dataset_download("kedarsai/bird-species-classification-220-categories")
+
+    # print("Path to dataset files:", path)
+
+    # specify the image size and the batch size to be used
     image_size = (224, 224)
     batch_size = 32
 
-
-
-
     test_dataset = tf.keras.utils.image_dataset_from_directory(
-        data_test_dir, labels='inferred', image_size= image_size, batch_size=batch_size
+        path + "/test", labels='inferred', image_size= image_size, batch_size=batch_size
     )
 
     train_dataset = tf.keras.utils.image_dataset_from_directory(
-        data_train_dir, labels='inferred', image_size= image_size, batch_size=batch_size
+        path + "/train", labels='inferred', image_size= image_size, batch_size=batch_size
     )
 
 
     print_class_labels(test_dataset, True)
     print_class_labels(train_dataset, False)
 
+    resnet_model = Sequential()
 
+    # we want to start our restNet50 model here with our own input/ output layers
+    model = ResNet50(weights='imagenet',
+                     include_top=False,
+                     pooling=None,
+                     input_shape=(224, 224, 3),
+                     input_tensor=None,
+                     classes = 200
+                     )
+
+    # for layer in model.layers:
+    #     layer.trainable = False
+
+    resnet_model.add(model)
+    resnet_model.add(GlobalAveragePooling2D())
+    resnet_model.add(Dense(512, activation='relu'))
+    resnet_model.add(Dense(200, activation='softmax'))
+
+    resnet_model.summary()
+
+    resnet_model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    epochs = 10
+    history = resnet_model.fit(train_dataset, epochs=epochs, validation_data=test_dataset)
+
+    resnet_model.save('my_model.keras')
 
     print(test_dataset)
 
-    # resnet50.preprocess_input = resnet50.preprocess_input
-
-    # input_tensor = tf.keras.Input(shape=(224, 224, 3))
-    #
-    # model = ResNet50(weights='imagenet',
-    #                  include_top=False,
-    #                  pooling='avg',
-    #                  input_shape=(224, 224, 3),
-    #                  classes=200,
-    #                  input_tensor = input_tensor,
-    #                  classifier_activation='softmax')
 
 
-
-    # model.summary()
-    print("Starting Main... ")
-
-
-
-    tf.keras.preprocessing.image_dataset_from_directory(
-
-    )
